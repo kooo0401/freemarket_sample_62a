d61f1ec557939e5b0eaea1b3cfc1d255a6ffe954
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-11-12 12:30:48 +0900

    add column to product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_025150) do
+ActiveRecord::Schema.define(version: 2019_11_12_032145) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 2019_11_12_025150) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.bigint "brand_id"
+    t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -74,5 +78,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_025150) do
   end
 
   add_foreign_key "images", "products"
+  add_foreign_key "products", "brands"
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
