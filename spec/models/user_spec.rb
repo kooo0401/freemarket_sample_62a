a2e4452a9eb480ec2473119f7604d8dbd22fdd82
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-11-13 14:25:26 +0900

    単体テストUser記述３

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe User do
   describe '#create' do
-    it "is valid with a name, email, password" do
+    it "is valid with a nickname, email, password" do
       user = build(:user)
       expect(user).to be_valid
     end
@@ -141,10 +141,154 @@ describe User do
     # パスワード
     # 6文字以下なら不適切
     it "is invalid with a password that has less than 6 characters " do
-      user = build(:user, password: "a2345", password_confirmation: "a2345")
+      user = build(:user, password: "a2345")
       user.valid?
-      expect(user.errors[:password]).to include("は6文字以上に設定して下さい。")
+      expect(user.errors[:password]).to include("は7文字以上に設定して下さい。")
     end
 
+    # 7文字以上に設定してください
+    it "is valid with a password that has more than 7 characters " do
+      user = build(:user, password: "a234567")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # 255文字以内に設定してください
+    it "is valid with a password that has less than 255 characters " do
+      user = build(:user, password: "aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa")#255文字
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # 256文字以上なら不適切    
+    it "is invalid with a password that has more than 256 characters " do
+      user = build(:user, password: "aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa12345aaaaa1")#256文字
+      user.valid?
+      expect(user.errors[:password][0]).to include("は255文字以下に設定して下さい。")
+    end
+
+    # パスワードに英字と数字が含まれているか
+    it "is valid with a password that Contains letters and numbers " do
+      user = build(:user, password: "a234567")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # 数字のみのため不適切
+    it "is invalid with a password except for numbers" do
+      user = build(:user, password: "1111111111")
+      user.valid?
+      expect(user.errors[:password][0]).to include("")
+    end
+
+    # 英字のみのため不適切
+    it "is invalid with a password except for alphabets" do
+      user = build(:user, password: "aaaaaaa")
+      user.valid?
+      expect(user.errors[:password][0]).to include("")
+    end
+
+    # パスワードに記号を入れても登録できる
+    it "is valid with a password that Contains letters and numbers and symbol " do
+      user = build(:user, password: "a123456!@#$%^&*)(=_-")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # name_kana
+    # 35文字以下に設定してください
+    it "is invalid with a name_kana that has more than 36 characters " do
+      user = build(:user, name_kana: "アアアアアアアアアアアアアアアアアアアアアアアアアアアアアアアアアアアア") # 36文字
+      user.valid?
+      expect(user.errors[:name_kana]).to include("は35文字以下に設定して下さい。")
+    end
+
+    # ひらがななら不適切
+    it "is invalid with a name_kana that katakana " do
+      user = build(:user, name_kana: "あ")
+      user.valid?
+      expect(user.errors[:name_kana][0]).to include("はカタカナで入力して下さい")
+    end
+
+    # tel
+
+    it "is valid with a tel that Phone number format " do
+      user = build(:user, tel: "08012345678")
+      user.valid?
+      expect(user).to be_valid
+    end
+
+    # 電話番号の形式が不適切でないか
+    it "is invalid with a tel that Phone number format " do
+      user = build(:user, tel: "a8012345678")
+      user.valid?
+      expect(user.errors[:tel][0]).to include("は有効でありません。")
+    end
+
+    # 郵便番号が不適切な形式になっていないか
+
+    it "is invalid with a zip that Postal code format " do
+      user = build(:user, zip: "a00-0000")
+      user.valid?
+      expect(user.errors[:zip]).to include("は有効でありません。")
+    end
+    
+    # 都道府県のhashが1~48であるか
+
+    it "is valid with a prefecture that has less than 48" do
+      user = build(:user, prefecture: "48")
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a prefecture that has more than 49" do
+      user = build(:user, prefecture: "49")
+      user.valid?
+      expect(user.errors[:prefecture])
+    end
+
+    it "is invalid with a prefecture only integer" do
+      user = build(:user, prefecture: "a")
+      user.valid?
+      expect(user.errors[:prefecture])
+    end
+
+    # 市区町村が50文字以下であるか
+
+    it "is valid with a city_name that has less than 50 characters " do
+      user = build(:user, city_name: "12345678901234567890123456789012345678901234567890") # 50文字
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a city_name that has more than 51 characters" do
+      user = build(:user, city_name: "123456789012345678901234567890123456789012345678901") # 51文字
+      user.valid?
+      expect(user.errors[:city_name]).to include("は50文字以内で入力してください")
+    end
+
+    # block_nameが100文字以下であるか
+
+    it "is valid with a block_name that has less than 100 characters " do
+      user = build(:user, block_name: "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890") # 100文字
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a block_name that has more than 101 characters" do
+      user = build(:user, block_name: "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901") # 101文字
+      user.valid?
+      expect(user.errors[:block_name]).to include("は100文字以下に設定して下さい。")
+    end
+
+    # bill_nameが100文字以下であるか
+
+    it "is valid with a bill_name that has less than 100 characters " do
+      user = build(:user, bill_name: "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890") # 100文字
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a bill_name that has more than 101 characters" do
+      user = build(:user, bill_name: "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901") # 101文字
+      user.valid?
+      expect(user.errors[:bill_name]).to include("は100文字以下に設定して下さい。")
+    end
   end
 end
