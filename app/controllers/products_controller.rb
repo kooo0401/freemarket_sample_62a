9d49f7741bbdf3373feb0070f07807c6a49937be
masakimurakami11 <monkey_basket_11@i.softbank.jp>
2019-11-17 17:32:58 +0900

    edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,9 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(1)
+    #実際にテーブルからid:1のproductを取得できているかの記述。
     # @product = Product.find(params[:id])
+    # 商品出品が可能になったら、一つ一つのproductからidで取得する。
     @grandchild = Category.find("#{@product.category_id}")
     @child = @grandchild.parent
     @parent = @child.parent
