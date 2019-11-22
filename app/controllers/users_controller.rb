de1901db03ae367255497169160f5fadeed11823
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-11-22 11:14:12 +0900

    mypage_correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'ユーザーを編集しました'
+    else
+      # 訂正要
+      render :profile_edit
+    end
+  end
+
   def logout_page
     @user = User.find(params[:id])
   end
@@ -15,11 +25,26 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def credit_edit
+  def credit_add
     @user = User.find(params[:id])
   end
 
   def confirmation_edit
     @user = User.find(params[:id])
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :nickname,
+      :self_introduction,
+      :zip,
+      :prefecture,
+      :city_name,
+      :block_name,
+      :bill_name,
+    )
+  end
+
 end
