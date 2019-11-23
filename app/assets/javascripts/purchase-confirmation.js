19757f732ba5f2d6c24dc246e1aedb231f34f2f2
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-11-23 15:43:55 +0900

    JS1

--- app/assets/javascripts/purchase-confirmation.js
+++ app/assets/javascripts/purchase-confirmation.js
@@ -1,5 +1,20 @@
 $(function(){  
+
   $( '#acdn-button' ).click( function(){
     $( '#acdn-target' ).slideToggle() ;
   });
+
+  $('input[name="point"]:radio').change(function(){
+    let selectedval = $(this).val();
+    if(selectedval == 1) {
+      console.log('aaa');
+      $('.form-box-tempolary__buy-accordion__accordion-parent__accordion-child__form-group__used-point').removeAttr('disabled');
+    }
+    else {
+      $('.form-box-tempolary__buy-accordion__accordion-parent__accordion-child__form-group__used-point').attr('disabled','disabled');
+    }
+  })
+
+
+
 });
