b5b27af63cc9420cbaa83a454ec91e6dff1e7b7e
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-11-23 16:14:33 +0900

    JS2

--- app/assets/javascripts/purchase-confirmation.js
+++ app/assets/javascripts/purchase-confirmation.js
@@ -6,14 +6,27 @@ $(function(){
 
   $('input[name="point"]:radio').change(function(){
     let selectedval = $(this).val();
-    if(selectedval == 1) {
+    if(selectedval == 2) {
       console.log('aaa');
       $('.form-box-tempolary__buy-accordion__accordion-parent__accordion-child__form-group__used-point').removeAttr('disabled');
+    }
+    else if(selectedval == 1) {
+      $('.form-box-tempolary__buy-accordion__accordion-parent__accordion-child__form-group__used-point').attr('disabled','disabled');
+      let price_to_paid = 3980- 200;
+      $('#data-buy-pay').html(price_to_paid);
     }
     else {
       $('.form-box-tempolary__buy-accordion__accordion-parent__accordion-child__form-group__used-point').attr('disabled','disabled');
     }
+
   })
+
+  $('.form-box-tempolary__buy-accordion__accordion-parent__accordion-child__form-group__used-point').on('keyup', function(){
+    let used_point = $(this).val();
+    let price_to_paid = 3980 - used_point;
+    $('#data-buy-pay').html(price_to_paid);
+  })
+
 
 
 
