f9c1255fcb0cbbc645e5c290710f98be8a32b0fa
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-11-25 10:01:01 +0900

    view-edit

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -13,11 +13,8 @@ class CardController < ApplicationController
       redirect_to action: "show"
     else
       customer = Payjp::Customer.create(
-      description: '登録テスト', #なくてもOK
-      email: current_user.email, #なくてもOK
       card: params['payjp-token'],
-      metadata: {user_id: current_user.id}
-      ) #念の為metadataにuser_idを入れましたがなくてもOK
+      )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to controller: "signup", action: "user_registration4"
