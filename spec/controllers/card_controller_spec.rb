5d984df7aa14f9330637eeb926f017af57cd3bba
kooo0401 <56294747+kooo0401@users.noreply.github.com>
2019-11-28 19:09:30 +0900

    edit

--- spec/controllers/card_controller_spec.rb
+++ spec/controllers/card_controller_spec.rb
@@ -3,6 +3,26 @@ require 'rails_helper'
 RSpec.describe CardController, type: :controller do
 
   describe "GET #new" do
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "GET #addition" do
+    it "renders the :addtion template" do
+      get :addition
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the :show template" do
+      get :show
+    end
+  end
+
+
+  describe "GET #new" do
     it "returns http success" do
       get :new
       expect(response).to have_http_status(:success)
@@ -15,7 +35,7 @@ RSpec.describe CardController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-  
+
   describe "GET #show" do
     it "returns http success" do
       get :show
