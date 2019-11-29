01d1f1a6cf53f23a8288a96d9ecc51bdeef6361e
hirakei1203 <56023847+hirakei1203@users.noreply.github.com>
2019-11-29 14:43:39 +0900

    test 4

--- spec/controllers/purchase_controller_spec.rb
+++ spec/controllers/purchase_controller_spec.rb
@@ -8,17 +8,26 @@ RSpec.describe PurchaseController, type: :controller do
     before do
       login user
     end
-    end
-    it "returns http success" do
-      get :index, params: { product_id: product.id }
-      expect(response).to have_http_status(:success)
+
+    context "purchase#indexを実行したとき" do
+      before do
+        get :index, params: { product_id: product.id }
+      end
+      it "redirect_to new_user_card_path" do
+        expect(response).to redirect_to new_user_card_path(user.id)
+      end
     end
   end
 
   describe "GET #done" do
-    it "returns http success" do
-      get :done
-      expect(response).to have_http_status(:success)
+    context "purchase#doneを実行したとき" do
+      before do
+        get :done, params: { product_id: product.id }
+      end
+
+      it "redirect_tp done_product_purchase_index" do
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
